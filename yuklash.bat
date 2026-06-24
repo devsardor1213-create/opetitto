05d251cc781bc4fef2a95bb267265059bbe50601
devsardor1213-create <devsardor1213@gmail.com>
2026-06-24 18:30:47 +0500

    Add files via upload

---
+++ yuklash.bat
@@ -0,0 +1,13 @@
+@echo off
+echo GitHubga yuklanmoqda... Iltimos, kuting...
+git add .
+git commit -m "Fix: WebApp uchun tekin GitHub Pages domeniga o'tildi"
+git push
+echo.
+echo ========================================================
+echo MUVAFFAQIYATLI YUKLANDI! Endi oxirgi ish qoldi:
+echo 1. https://github.com/devsardor1213-create/opetitto ga kiring
+echo 2. "Settings" -> "Pages" ga o'ting
+echo 3. "Branch" ni 'main' qilib, 'Save' tugmasini bosing!
+echo ========================================================
+pause
